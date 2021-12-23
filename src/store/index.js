f903b73536a417ad3529719a5a11982264df9ece
Payton-Lee <persurejonas1202@163.com>
2021-12-23 08:59:44 +0800

    ToDolist

---
+++ src/store/index.js
@@ -0,0 +1,63 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import axios from 'axios'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    // 所有的任务列表
+    list: [],
+    inputValue: 'aaa',
+    // 下一个id
+    nextId : 5
+  },
+  mutations: {
+    initList(state, list) {
+      state.list = list
+    },
+    // 为 store 中的inputValue 赋值
+    setInputValue(state, val) {
+      state.inputValue = val
+    },
+    // 添加列表项目
+    addItem(state) {
+      const obj = {
+        id: state.nextId,
+        info: state.inputValue.trim(),
+        done: false
+      }
+      state.list.push(obj)
+      state.nextId++
+      state.inputValue = ''
+    },
+    // 根据id删除对应的任务事项
+    removeItem(state, id) {
+      // 根据id查找对应项的索引
+      const i = state.list.findIndex(x => x.id === id)
+      // 根据索引，删除对应的元素
+      if(i !== -1) {
+        state.list.splice(i, 1)
+      }
+    },
+    // 修改列表项的选中状态
+    changeStatus(state, param) {
+      const i = state.list.findIndex(x => x.id === param.id)
+      if( i !== -1) {
+        state.list[i].done = param.status
+      }
+    }
+  },
+  actions: {
+  },
+  actions: {
+    getList(context) {
+      axios.get('/list.json').then(({ data })=>{
+        console.log(data);
+        context.commit('initList', data)
+      })
+    }
+  },
+  modules: {
+  }
+})
